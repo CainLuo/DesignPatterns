d893d7ff1da54e2f2d05e0f7a9df3de2bedf4f0f
CainLuo <350116542@qq.com>
2021-10-27 18:21:16 +0800

    Add Adapter Pattern Project

---
+++ 8.AdapterPattern/8.AdapterPattern/Modules/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  8.AdapterPattern
+//
+//  Created by YYKJ0048 on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
